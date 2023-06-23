75810336db4e3a5d469df1689d42c0f1066f9208
Lewis <lewis.murgor@student.moringaschool.com>
2023-06-23 12:12:15 +0300

    create save and delete methods for menu class

--- restaurant/models.py
+++ restaurant/models.py
@@ -10,3 +10,12 @@ class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+    
+    def save_menu(self):
+        self.save()
+
+    def delete_menu(self):
+        self.delete()
